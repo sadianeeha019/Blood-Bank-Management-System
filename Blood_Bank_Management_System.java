08a69cf2ba35816d774d09c024f59d2291ff1739
Sadia Sabrin Neha <107767080+sadianeeha019@users.noreply.github.com>
2025-05-31 22:48:50 +0600

    Add files via upload

---
+++ Blood_Bank_Management_System.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package blood_bank_management_system;
+
+/**
+ *
+ * @author User
+ */
+public class Blood_Bank_Management_System {
+    private static int i;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*loading Loading= new loading();
+        Loading.setVisible(true);
+        
+        try{
+            for(int i=0; i<=100; i++)
+            {
+                Thread.sleep(40);
+                Loading.loadingnum.setText(Integer.toString(i)+"%");
+                Loading.loadingbar.setValue(i);
+        }
+        }
+        catch(Exception e)
+        {
+            
+        }*/
+    }
+    
+}
